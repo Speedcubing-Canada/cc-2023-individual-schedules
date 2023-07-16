707248ee2159fbb36c25bc1a6980d53eccab30b8
Jon Esparaz <48423418+JonEsparaz@users.noreply.github.com>
2023-07-16 11:47:45 -0400

    Revert "fix python script to align with new file tree"
    This reverts commit 57a8bb2a387502e3e4c3bef770eef6ca48107c69.

--- make-individuals-html-docs.py
+++ make-individuals-html-docs.py
@@ -4,7 +4,7 @@ import shutil
 html_footer = '</body>\n</html>'
 
 if __name__ == '__main__':
-    with open('./index.html', 'r') as f:
+    with open('./public/index.html', 'r') as f:
         start_new_file = False
         is_writing = False
         current_write_file = ''
@@ -12,7 +12,7 @@ if __name__ == '__main__':
             start_new_file = 'name-link' in line
             if start_new_file:
                 result = re.search(r'.+href=\"(.+)\">(.+)<.+', line)
-                current_write_file = result.group(1)
+                current_write_file = f'./public/{result.group(1)}'
                 with open(current_write_file, 'w+') as person_file:
                     html_head = f'<!DOCTYPE html>\n<html>\n<head>\n<meta charset="utf-8">\n<meta name="viewport" content="width=device-width">\n<title>{result.group(2)}</title>\n<link href="style.css" rel="stylesheet" type="text/css" />\n<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato"/>\n</head>\n<body>\n'
                     person_file.write(html_head)
@@ -25,4 +25,4 @@ if __name__ == '__main__':
             if '</table>' in line: # stop writing at end of table
                 is_writing = False
 
-    shutil.copy('./style.css', './person') # loading css from a lower path is difficult - just duplicate the stylesheet in the persons/ dir
+    shutil.copy('./public/style.css', './public/person') # loading css from a lower path is difficult - just duplicate the stylesheet in the persons/ dir
